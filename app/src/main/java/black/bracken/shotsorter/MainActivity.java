a155501d19dbd57aae706485b8efd72d361a4170
BlackBracken <unicroak@icloud.com>
2018-08-03 15:42:45 +0900

    initialize project

---
+++ app/src/main/java/black/bracken/shotsorter/MainActivity.java
@@ -0,0 +1,19 @@
+package black.bracken.shotsorter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * ShotSorter
+ *
+ * @author BlackBracken
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
